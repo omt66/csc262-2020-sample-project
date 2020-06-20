d6948bca3d7960965da60da45809e109d26bda03
omt66 <omt66@yahoo.com>
2020-06-19 23:42:12 -0700

    Initial commit

---
+++ src/main/java/omt/finalprjpart1/Grid.java
@@ -0,0 +1,55 @@
+package omt.finalprjpart1;
+
+/**
+ * Grid class that represents a 2D grid.
+ * 
+ * @author omt
+ */
+public class Grid {
+    char[][] grid;
+    int nofRows = 10;
+    int nofCols = 10;
+
+    public Grid() {
+        init();
+    }
+
+    public Grid(int nofRows, int nofCols) {
+        this.nofRows = nofRows;
+        this.nofCols = nofCols;
+        init();
+    }
+
+    private void init() {
+        grid = new char[nofCols][nofRows];
+        for (int y = 0; y < nofRows; y++) {
+            for (int x = 0; x < nofCols; x++) {
+                grid[x][y] = '.';
+            }
+        }
+    }
+
+    public void setValueAt(int x, int y, char val) {
+        grid[x][y] = val;
+    }
+
+    public void print2D() {
+        System.out.println("--------------------------------------------------");
+        for (int y = 0; y < nofRows; y++) {
+            for (int x = 0; x < nofCols; x++) {
+                char ch = grid[x][y];
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Grid g = new Grid(25, 25);
+        
+        g.setValueAt(10, 8, 'B');
+        g.setValueAt(22, 4, 'C');
+        g.print2D();
+    }
+}
---
+++ src/main/java/omt/finalprjpart1/Robot.java
@@ -0,0 +1,105 @@
+package omt.finalprjpart1;
+
+/**
+ * TODO Explain what it does...
+ * 
+ * @author omt
+ */
+public class Robot {
+
+    private int x;
+    private int y;
+    private char payload;
+    private String name;
+    private Grid grid;
+
+    public Robot(String name) {
+        this.name = name;
+        x = 0;
+        y = 0;
+        payload = ' ';
+    }
+
+    /**
+     * Picks up the item from the grid at location x & y
+     * 
+     * @param locationX 
+     * @param locationY
+     */
+    boolean pickUp(int locationX, int locationY) {
+        // TODO: Implement this
+        return false;
+    }
+
+    // TODO Explain what it does...
+    void dropOff(int lx, int ly) {
+        // TODO: Implement this
+    }
+
+    // TODO Explain what it does...
+    void moveRight() {
+        // TODO: Implement this
+    }
+
+    // TODO Explain what it does...
+    void moveLeft() {
+        // TODO: Implement this
+    }
+
+    // TODO Explain what it does...
+    void moveUp() {
+        // TODO: Implement this
+    }
+
+    // TODO Explain what it does...
+    void moveDown() {
+        // TODO: Implement this
+    }
+
+    // TODO Explain what it does...
+    boolean moveTo(int locationX, int locationY) {
+        if (locationX > grid.nofCols || locationY > grid.nofRows) {
+            return false;
+        }
+        x = locationX;
+        y = locationY;
+
+        return true;
+    }
+
+    // TODO Explain what it does...
+    void placeItem(int locationX, int locationY, char item) {
+        grid.setValueAt(locationX, locationY, item);
+    }
+
+    // TODO Explain what it does...
+    public void useGrid(Grid grid) {
+        this.grid = grid;
+    }
+    
+    // TODO Explain what it does...
+    void printLocation() {
+        String formatStr = "Robot %s is at %d, %d\n";
+        System.out.printf(formatStr, name, x, y);
+    }
+
+    public static void main(String[] args) {
+        Robot r1 = new Robot("R1");
+        Robot r2 = new Robot("R2");
+        Grid grid = new Grid(25, 25);
+
+        grid.setValueAt(10, 8, 'B');
+        grid.setValueAt(22, 4, 'C');
+
+        r1.useGrid(grid);
+        r2.useGrid(grid);
+
+        r1.moveTo(23, 24);
+        r2.moveTo(15, 3);
+        
+        r1.printLocation();
+        r2.printLocation();
+
+        grid.print2D();
+    }
+}
